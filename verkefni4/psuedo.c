a9366f854e3507418559fce6ced60bc70910d485
Benedikt Aron Sigurþórsson <benediktsi228@nemi.tskoli.is>
2018-04-30 15:48:36 +0000

    Completed Verk4

--- verkefni4/psuedo.c
+++ verkefni4/psuedo.c
@@ -1,10 +1,4 @@
-bool pause = false;
-bool reversing = false;
-Timer turnTimer;
-Timer driveTimer;
-unsigned int maxTurnTime = 10;
-unsigned int maxDriveTime = 5;
-unsigned int minDist = 20;
+unsigned int minDist = 35;
 
 void drive(speed=80, key, reversed=false)
 {
@@ -31,36 +25,9 @@ task main
 	startTask(button);
 	while(true)
 	{
-		if(!pause)
+		if(sensorValue(sonarSensor) > minDist)
 		{
-			if(sonicSensorDistance <= minDist)
-			{
-				restart driveTimer with maxDriveTime as milli
-				if(turnTime >= maxTurnTime)
-				{
-					if(!reversing)
-					{
-						reversing = true;
-						minDist += 5;
-					}
-					if(drive(1, reversed=true))
-					{
-						restart turnTimer with maxTurnTime as milli
-						reversing = false;
-					}
-				}
-				else
-				{
-					reversing = false;
-					turn(1);
-				}
-			}
-			else
-			{
-				reversing = false;
-				drive();
-				restart turnTimer with maxTurnTime as milli
-			}
+			
 		}
 	}
 }
